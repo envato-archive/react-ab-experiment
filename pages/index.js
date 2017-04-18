5b91e34b3974e1df69634f76fca181e0a3b97db7
Toan Nguyen <toan@graduate.uwa.edu.au>
2017-04-18 13:33:41 +1000

    Setup nextjs for development

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactABExperiment from './ReactABExperiment'
+
+export default () => (
+  <ReactABExperiment>
+    <Variant id=0>
+    </Variant>
+
+  </ReactABExperiment>
+)
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class ReactABExperiment extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  componentDidMount() {
+  }
+
+  render () {
+
+  }
+}
+
+export default ReactABExperiment
